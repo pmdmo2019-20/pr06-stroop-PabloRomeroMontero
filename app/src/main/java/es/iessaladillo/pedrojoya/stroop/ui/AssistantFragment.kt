4a5cc5cabc3075ebfc22ef5e3f4e8eca9b7ae6d0
Ricardo Pedrosa <rickpedrosa94@gmail.com>
2020-02-27 23:04:15 +0100

    Erorr

---
+++ app/src/main/java/es/iessaladillo/pedrojoya/stroop/ui/AssistantFragment.kt
@@ -0,0 +1,7 @@
+package es.iessaladillo.pedrojoya.stroop.ui
+
+import androidx.fragment.app.Fragment
+import es.iessaladillo.pedrojoya.stroop.R
+
+class AssistantFragment : Fragment(R.layout.assistant_fragment) {
+}
--- app/src/main/java/es/iessaladillo/pedrojoya/stroop/ui/MainActivity.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/stroop/ui/MainActivity.kt
@@ -1,10 +1,46 @@
 package es.iessaladillo.pedrojoya.stroop.ui
 
+import android.content.Context
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+import es.iessaladillo.pedrojoya.stroop.R
+import es.iessaladillo.pedrojoya.stroop.base.OnToolbarAvailableListener
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnToolbarAvailableListener {
+    lateinit var context: Context
 
-    // TODO
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        context = this
+    }
+
+
+
+    private val navController: NavController by lazy {
+        findNavController(R.id.navHostFragment)
+    }
+
+    val appBarConfiguration: AppBarConfiguration = AppBarConfiguration(navController.graph)
+
+
+    override fun onToolbarCreated(toolbar: Toolbar) {
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onToolbarDestroyed() {
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 
 }
